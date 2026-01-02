9f7c51acd4f7d6cc56ecfabc5152afb940387355
8Autumn8 <vela.belinda.k@gmail.com>
2026-01-02 03:35:33 -0500

    updated connection to db

--- database/db.py
+++ database/db.py
@@ -2,12 +2,16 @@ import sqlite3
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
+DB_URL = os.getenv("DB_URL")
 
 def get_db():
-    conn = sqlite3.connect("house_manager.db", timeout=10)
+    conn = psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
+    #conn = sqlite3.connect(DB_PATH, timeout=10)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA journal_mode=WAL;")
     return conn
