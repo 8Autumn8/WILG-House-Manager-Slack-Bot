ecf8c04fc09497c6ec18da74126ad6e820466b1e
Belinda Vela <vela.belinda.k@gmail.com>
2026-02-07 23:07:14 -0500

    modified submission comments again

--- app.py
+++ app.py
@@ -105,7 +105,8 @@ def submit_hour_api():
             "response_type": "ephemeral",
             "text": f"❌ Could not find witness user: {commands[3]}"
         }), 400
-    comments = ' '.join(commands[4::]) if len(commands) > 4 else ""
+    comments = " ".join(commands[4:]) if len(commands) > 4 else ""
+    print("comments:", comments)
     submission_time = data.get("submission_time", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     username = data.get('user_name')
     # Immediately ACK Slack
